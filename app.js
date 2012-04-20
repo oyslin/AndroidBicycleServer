96dbf6d1c0ac04004daeb0dfd689597f3b1738a5
Walt Ouyang <oyslin@163.com>
2012-04-20 16:53:49 +0800

    modified:   app.js 	modified:   public/styles/style.css 	modified:   views/feedback.jade

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express'),
 	app = express.createServer(),
 	mongo = require('mongoskin'),
+	util = require('util'),
 	mongodb = mongo.db('127.0.0.1:27017/bicycleproxy'),
 	bicycleInfo = require('./server/bicycleinfo');
 	
@@ -63,7 +64,16 @@ app.get('/updateversioninfo', function(req, res){
 });
 
 app.get('/feedback', function(req, res){
-	res.render('feedback.jade', {title: 'Feedback'});
+	var feedback = mongodb.collection('feedback');
+	feedback.find({}).toArray(function(err, result){
+		if(err){
+			res.redirect('/error');
+		}else{
+			// console.log('result = ' + util.inspect(result));
+			res.render('feedback.jade', {title:'AndroidBicycle Feedback', feedbackArray : result});
+		}
+	});
+	
 });
 
 app.get('/error', function(req, res){
@@ -104,10 +114,10 @@ app.post('/versioninfo', function(req, res){
 app.post('/feedback', function(req, res){
 	var feedbackMsg = req.body.msg;
 	var feedback = mongodb.collection('feedback');
-	feedback.inert({appName:'AndroidBicycle', feedbackMsg : feedbackMsg}, function(err, result){
+	feedback.inert({appName:'AndroidBicycle', feedbackMsg : feedbackMsg, uploadTime : new Date().toLocaleString}, function(err, result){
 		if(err){
 			res.redirect('/error');
-		}else{			
+		}else{
 			if(result){
 				res.redirec('/');
 			}else{
